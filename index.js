455b3a648aecbb054961592ff7ce31d94bd7f1b6
marvio72 <mruvalcaba@oversistemas.com>
2021-03-18 21:35:16 -0600

    Conectando a MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 // Crear el servidor
 const app = express();
+
+// Conectar a mongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/veterinaria', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
 // Puerto y crear Servidor
 app.listen(4000, () => {
