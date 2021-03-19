e5acc6f46f95820c1ac40f1dc080da5406bdc1b6
marvio72 <mruvalcaba@oversistemas.com>
2021-03-18 23:07:33 -0600

    Leyendo los contenidos de una petición con body-parser

--- controllers/pacienteControllers.js
+++ controllers/pacienteControllers.js
@@ -1,6 +1,6 @@
 // Cuando se crea un nuevo cliente
 exports.nuevoCliente = (req, res, next) => {
-  // TODO : Insertar en la base de datos
+  console.log(req.body);
 
   res.json({ mensaje: 'El cliente se agregó correctamente' });
 };
